01eabd25cb854c81c0e32ffc2453ac4da95472aa
Rohit <rgupta6205@gmail.com>
2024-06-19 09:17:14 +0530

    first commit

---
+++ api/students.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const {
+  CreateStudent,
+  GetStudents,
+  GetStudentById,
+  DeleteStudent,
+  DeleteStudents,
+  UpdateStudent,
+} = require("../controller/StudentController");
+
+router.get("/", GetStudents);
+router.post("/create-student", CreateStudent);
+router.put("/update-student/:id", UpdateStudent);
+router.get("/get-student-by-id/:id", GetStudentById);
+router.delete("/delete-student/:id", DeleteStudent);
+router.delete("/", DeleteStudents);
+
+module.exports = router;
