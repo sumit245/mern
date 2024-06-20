8b75ebf017957656294b42d7980653aeeaf60c52
Bittu75469 <bittukr4715@gmail.com>
2024-06-20 16:24:50 +0530

    changed Teacher, Student and Subject part

--- model/StudentModel.js
+++ model/StudentModel.js
@@ -3,28 +3,119 @@ const mongoose = require("mongoose");
 const Students = new mongoose.Schema(
   {
     student_id: String,
-    first_name: String,
-    last_name: String,
-    date_of_birth: String,
-    gender: String,
-    blood_group: String,
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    date_of_birth: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
+    gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+    blood_group: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
     email: String,
-    phone_number: String,
-    address: String,
+    phone_number: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
+    address: { type: String, required: true },
     student_photo: String,
-    admission_id: String,
-    admission_date: String,
+    admission_id: {
+      type: Number,
+      min: [4, "Must be at least"],
+      required: true,
+    },
+    admission_date: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
     course: String,
-    hostal_number: String,
-    bus_number: String,
-    bus_route_number: String,
-    bus_driver_name: String,
-    bus_driver_phone_number: String,
-    fathers_name: String,
-    mothers_name: String,
-    fathers_phone_number: String,
-    mothers_phone_number: String,
-    emergency_contact_number: String,
+    hostel_number: {
+      type: Number,
+      min: [2],
+      required: true,
+    },
+    bus_number: {
+      type: String,
+      min: [10],
+      required: true,
+    },
+    bus_route_number: {
+      type: Number,
+      min: [2],
+      required: true,
+    },
+    bus_driver_name: {
+      type: String,
+      required: true,
+    },
+    bus_driver_phone_number: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
+    fathers_name: {
+      type: String,
+      required: true,
+    },
+    mothers_name: {
+      type: String,
+      required: true,
+    },
+    fathers_phone_number: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
+    mothers_phone_number: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
+    emergency_contact_number: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
   },
   {
     timestamps: { createdAt: true, updatedAt: true },
--- model/TeacherModel.js
+++ model/TeacherModel.js
@@ -2,30 +2,120 @@ const mongoose = require("mongoose");
 
 const Teachers = new mongoose.Schema(
   {
-    employee_id: String,
-    first_name: String,
-    last_name: String,
-    date_of_brith: String,
-    gender: String,
-    blood_group: String,
+    employee_id: {
+      type: Number,
+      min: [4],
+      required: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    date_of_birth: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
+    gender: {
+      type: "object",
+      properties: {
+        gender: {
+          type: "string",
+          enum: ["male", "female", "transgender"],
+        },
+      },
+      required: ["gender"],
+    },
+
+    blood_group: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
     nationality: String,
     religion: String,
     marital_status: String,
     email_id: String,
-    phone_number: String,
+    phone_number: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
     photo: String,
-    address: String,
+    address: { type: String, required: true },
+    currentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
     job_title: String,
     department: String,
-    joining_date: String,
-    allotment_class: String,
-    allotment_section: String,
-    account_number: String,
-    ifsc_code: String,
+    joining_date: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
+    allotment_class: {
+      type: Number,
+      enum: ["1", "2", "3", "4", "5", "6", "7", "8"],
+      required: true,
+    },
+    allotment_section: {
+      type: String,
+      enum: ["A", "B", "C", "D"],
+      required: true,
+    },
+    account_number: {
+      type: Number,
+      min: [14, "Account Number not less than fourteen digit"],
+      required: true,
+    },
+    ifsc_code: {
+      type: String,
+      min: [10, "ifsc  code not less than ten digit"],
+      required: true,
+    },
     bank_name: String,
     salary_amount: String,
-    epfo_account_number: String,
-    emergency_contact_number: String,
+    epfo_account_number: {
+      type: Number,
+      min: [14, " epfo Account Number not less than fourteen digit"],
+      required: true,
+    },
+    emergency_contact_number: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
   },
   {
     timestamps: { createdAt: true, updatedAt: true },
