85716a44e5adb61ef6c46a2f4c2ee477369c46ab
Rohit <rgupta6205@gmail.com>
2024-06-20 16:40:41 +0530

    modifiy Model

--- model/SubjectModel.js
+++ model/SubjectModel.js
@@ -2,18 +2,92 @@ const mongoose = require("mongoose");
 
 const Subjects = new mongoose.Schema(
   {
-    subjectName: String,
-    subjectCode: String,
-    subjectAssigned_by: String,
-    subjectPeriod: String,
-    examinationDate: String,
-    examMode: String,
-    resultPublicationDate: String,
-    marks: String,
-    gread: String,
-    passingStatus: String,
-    certificateDistributionDate: String,
-    certificateNumber: String,
+    subjectName: {
+      type: String,
+      required: true,
+    },
+    subjectCode: {
+      type: Number,
+      required: true,
+    },
+    subjectAssignedBy: String,
+    subjectPeriod: {
+      type: Number,
+      required: true,
+    },
+    examinationDate: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
+
+    examMode: { type: String, enum: ["Offline", "Online"], required: true },
+    resultPublicationDate: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
+
+    marks: {
+      type: Number,
+      required: true,
+    },
+    grade: {
+      type: String,
+      enum: ["A", "B", "C", "D", "E", "O"],
+      required: true,
+    },
+    passingStatus: {
+      type: String,
+      required: true,
+    },
+    certificateDistributionDate: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
   },
   {
     timestamps: { createdAt: true, updatedAt: true },
--- model/TeacherModel.js
+++ model/TeacherModel.js
@@ -2,30 +2,91 @@ const mongoose = require("mongoose");
 
 const Teachers = new mongoose.Schema(
   {
-    employeeid: String,
-    firstName: String,
-    lastName: String,
-    dateOfBrith: String,
-    gender: String,
-    bloodGroup: String,
-    nationality: String,
-    religion: String,
-    maritalStatus: String,
+    employeeId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
+    gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+    bloodGroup: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
+    nationality: { type: String, required: true },
+    religion: { type: String, required: true },
+    maritalStatus: {
+      type: String,
+      enum: ["Single", "Married", "Divorced", "Widowed"],
+      required: true,
+    },
     emailId: String,
-    phoneNumber: String,
+    mobileNo: {
+      type: Number,
+      min: [10, "Mobile Number not less than ten digit"],
+      required: true,
+    },
     photo: String,
-    address: String,
-    jobTitle: String,
-    department: String,
-    joiningDate: String,
-    allotmentClass: String,
-    allotmentSection: String,
-    accountNumber: String,
-    ifscCode: String,
-    bankName: String,
-    salaryAmount: String,
-    accountNumber: String,
-    emergencyContactNumber: String,
+    address: { type: String, required: true },
+    jobTitle: { type: String, required: true },
+    department: { type: String, required: true },
+    joiningDate: {
+      type: "object",
+      properties: {
+        day: {
+          type: "integer",
+          minimum: 1,
+          maximum: 31,
+        },
+        month: {
+          type: "integer",
+          minimum: 1,
+          maximum: 12,
+        },
+        year: {
+          type: "integer",
+        },
+      },
+      required: ["day", "month", "year"],
+    },
+    allotmentClass: { type: Number, required: true },
+    allotmentSection: { type: String, required: true },
+    accountNumber: { type: Number, required: true },
+    ifscCode: { type: String, required: true },
+    bankName: { type: String, required: true },
+    salaryAmount: { type: Number, required: true },
+    accountNumber: { type: Number, required: true },
+    emergencyContactNumber: {
+      type: Number,
+      min: [10, "Mobile Number is not less then 10 digits"],
+      required: true,
+    },
   },
   {
     timestamps: { createdAt: true, updatedAt: true },
